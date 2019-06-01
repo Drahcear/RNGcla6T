2a1301bf6d2a1702f692b89b78da437b271c7d78
richard.lay <richard.lay@epita.fr>
2019-06-01 09:48:10 +0200

    top
    player

--- RNG-master/Random-Name-Game-master (1)/Random-Name-Game-master/Projet/Assets/Script/BackDash.cs
+++ RNG-master/Random-Name-Game-master (1)/Random-Name-Game-master/Projet/Assets/Script/BackDash.cs
@@ -16,11 +16,13 @@ namespace BestMasterYi
         private float BdashTime;
         public float startDashTime;
         private int direction;
+        private Animator Animé;    
 
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
             BdashTime = startDashTime;
+            Animé = GetComponent<Animator>();
         }
 
         // Update is called once per frame
@@ -28,25 +30,35 @@ namespace BestMasterYi
         {
             if (direction == 0)
             {
+                
                 if (Input.GetKey(KeyCode.B))
                 {
+                    
                     direction = 1;
                 }
             }
             else
             {
                 if (BdashTime <= 0)
-                {
+                { 
+                    Animé.SetTrigger("Backdash");
                     direction = 0;
                     Bdash = startDashTime;
                     rb.velocity = Vector2.zero;
+                    
+                    
+                    
                 }
 
                 else
                 {
                     BdashTime -= Time.deltaTime;
                     if (direction == 1)
+                    {
                         rb.velocity = new Vector2(Bdash, 0);
+                        
+                        
+                    }
                 }
             }
         }
