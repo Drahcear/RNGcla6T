ec880f5490171916487e194a5ff1c5fd34662e10
Dyabaulau <41041097+Dyabaulau@users.noreply.github.com>
2019-06-02 09:59:09 +0100

    animations demon et leviers

---
+++ RNG-master/Random-Name-Game-master (1)/Random-Name-Game-master/Projet/Assets/Script/TriggerMulti.cs
@@ -0,0 +1,45 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace BestMasterYi
+{
+    public class TriggerMulti : MonoBehaviourPun
+    {
+
+        public GameObject Collider;
+        public bool test;
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                if (test)
+                {
+                    PhotonView photonView = PhotonView.Get(this);
+                    photonView.RPC("Trigger", RpcTarget.All, null);
+                }
+                else
+                {
+                    PhotonView photonView = PhotonView.Get(this);
+                    photonView.RPC("AntiTrigger", RpcTarget.All, null);
+                }
+
+                test = !test;
+            }            
+            
+        }
+
+        [PunRPC]
+        void Trigger()
+        {
+            Collider.SetActive(true);
+            
+        }
+        [PunRPC]
+        void AntiTrigger()
+        {
+            Collider.SetActive(false);
+            
+        }
+    }
+}
